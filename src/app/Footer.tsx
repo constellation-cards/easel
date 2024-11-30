914394d790eb9600be8bb77441f507737ab58d8d
Bill Garrett <garrett@astralfrontier.org>
2024-11-30 11:28:37 -0800

    Add cards.json and cards.pdf

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -3,12 +3,23 @@ export default function Page() {
     <footer className="footer">
       <div className="columns">
         <div className="column">
-          <a
-            href="https://steamcommunity.com/sharedfiles/filedetails/?id=3080002958"
-            target="_blank"
-          >
-            Play online: Tabletop Simulator
-          </a>
+          <ul>
+            <li>
+              <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3080002958" target="_blank">
+                Play online: Tabletop Simulator
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="column">
+          <ul>
+            <li>
+              <a href="/cards.pdf">cards.pdf</a> (printable cards)
+            </li>
+            <li>
+              <a href="/cards.json">cards.json</a> (raw data)
+            </li>
+          </ul>
         </div>
       </div>
     </footer>
