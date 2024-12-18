81ae74929abfa0ae9f43bf531c667ca2db6c9ee0
Bill Garrett <garrett@astralfrontier.org>
2024-12-18 01:59:15 -0800

    Add nix build step

--- flake.nix
+++ flake.nix
@@ -12,10 +12,23 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        packages.default = pkgs.buildNpmPackage {
+          name = "constellation-cards-easel";
+          buildInputs = with pkgs; [
+            nodejs_22
+          ];
+          src = self;
+          npmDepsHash = "sha256-rCr7RCtwe2M1Gx9N9wFG1IWrY+T00xpb8Oj0U707MQQ=";
+          installPhase = ''
+            mkdir $out
+            cp -r out/* $out/
+            echo "Did it" > $out/did-it
+          '';
+        };
         devShell = pkgs.mkShell {
           name = "easel";
-          packages = [
-            pkgs.nodejs_22
+          packages = with pkgs; [
+            nodejs_22
           ];
         };
       }
