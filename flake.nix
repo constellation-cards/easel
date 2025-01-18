0cd6260356804393dd33cb78552d034fa07c0454
Bill Garrett <garrett@astralfrontier.org>
2025-01-18 09:19:37 -0800

    Add build targets for JSON, PDF, and CSV

--- flake.nix
+++ flake.nix
@@ -24,12 +24,12 @@
         packages.nodemodules = pkgs.buildNpmPackage {
           name = "constellation-cards-easel-nodemodules";
           nativeBuildInputs = with pkgs; [ nodejs_22 ];
-          src = self;
+          src = nixpkgs.lib.sources.sourceByRegex self ["^package(|-lock)\.json$"];
           npmDepsHash = "sha256-NHIVSIUSZjbBQQa+cB/XRgFUDbCybKNVKpE5AhsXcjE=";
           npmBuildScript = "true";
           installPhase = ''
             mkdir $out
-            cp -r node_modules/* $out/
+            cp -a node_modules/. $out/
           '';
         };
         packages.nextjs = pkgs.stdenv.mkDerivation {
@@ -37,12 +37,56 @@
           nativeBuildInputs = easeldeps ++ [ self.packages.${system}.nodemodules ];
           src = self;
           buildPhase = ''
-            ln -s ${self.packages.${system}.nodemodules} node_modules
+            cp -a ${self.packages.${system}.nodemodules}/. node_modules/
+            ls -la
             npm run testbuild
           '';
           installPhase = ''
             mkdir $out
             cp -r out/* $out/
+          '';
+        };
+        packages.json = pkgs.stdenv.mkDerivation {
+          name = "constellation-cards-easel-json";
+          nativeBuildInputs = easeldeps ++ [ self.packages.${system}.nodemodules ];
+          src = self;
+          buildPhase = ''
+            cp -a ${self.packages.${system}.nodemodules}/. node_modules/
+            npm run out-json cards.json
+          '';
+          installPhase = ''
+            mkdir $out
+            cp cards.json $out/
+          '';
+        };
+        packages.pdf = pkgs.stdenv.mkDerivation {
+          name = "constellation-cards-easel-pdf";
+          nativeBuildInputs = easeldeps ++ [ self.packages.${system}.nodemodules self.packages.${system}.json ];
+          src = self;
+          buildPhase = ''
+            cp -a ${self.packages.${system}.nodemodules}/. node_modules/
+            npm run out-tex ${self.packages.${system}.json}/cards.json cards.tex
+            export OSFONTDIR=$PWD/fonts
+            mtxrun --generate
+            mtxrun --script fonts --reload
+            context cards.tex --purgeall
+          '';
+          installPhase = ''
+            mkdir $out
+            cp cards.pdf $out/
+          '';
+        };
+        packages.csv = pkgs.stdenv.mkDerivation {
+          name = "constellation-cards-easel-csv";
+          nativeBuildInputs = easeldeps ++ [ self.packages.${system}.nodemodules self.packages.${system}.json ];
+          src = self;
+          buildPhase = ''
+            cp -a ${self.packages.${system}.nodemodules}/. node_modules/
+            npm run out-csv ${self.packages.${system}.json}/cards.json cards.csv
+          '';
+          installPhase = ''
+            mkdir $out
+            cp cards.csv $out/
           '';
         };
         packages.default = pkgs.buildNpmPackage {
